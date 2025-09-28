6aa6e1ee996b34edff252ec46f6c1ca9e93e8433
Boris Ciobirca <boris.chobyrka@gmail.com>
2025-09-28 23:35:07 +0100

    connected tg bot to express

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import express from "express";
 import { Telegraf } from "telegraf";
 
 const bot = new Telegraf(process.env.BOT_TOKEN || "");
+const PORT = process.env.PORT || 443;
+const DOMAIN = process.env.RAILWAY_PUBLIC_DOMAIN || "";
 const app = express();
 
 app.use(express.json());
+app.use(await bot.createWebhook({ domain: DOMAIN }));
 
-const PORT = 443;
+bot.start((ctx) => ctx.reply("Hello World!"));
 
-app.listen(PORT, async () => {
-  console.log(process.env.RAILWAY_PUBLIC_DOMAIN, process.env.PORT);
-});
+app.listen(PORT, () => console.log("Listening on PORT", PORT));
