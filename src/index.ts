0c81ad562bf6644348e767f188f5c5ccbf5c6bcf
Boris Ciobirca <boris.chobyrka@gmail.com>
2025-09-28 23:37:47 +0100

    migrated to bun.serve

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
-import express from "express";
+import { serve } from "bun";
 import { Telegraf } from "telegraf";
 
 const bot = new Telegraf(process.env.BOT_TOKEN || "");
-const PORT = process.env.PORT || 443;
+const PORT = Number(process.env.PORT || 443);
 const DOMAIN = process.env.RAILWAY_PUBLIC_DOMAIN || "";
-const app = express();
 
-app.use(express.json());
-app.use(await bot.createWebhook({ domain: DOMAIN }));
+bot.start((ctx) => ctx.reply(`Running on ${DOMAIN}:${PORT}!`));
 
-bot.start((ctx) => ctx.reply("Hello World!"));
+const server = serve({
+  port: PORT,
+  async fetch(req) {
+    if (req.method === "POST") {
+      const body = await req.json();
+      await bot.handleUpdate(body).catch(console.error);
+      return new Response("OK");
+    }
+    return new Response("Hello from Bun server");
+  },
+});
 
-app.listen(PORT, () => console.log("Listening on PORT", PORT));
+console.log("Bun server running on port", PORT);
+
+if (DOMAIN) {
+  bot.telegram
+    .setWebhook(`${DOMAIN}/bot${process.env.BOT_TOKEN}`)
+    .then(() =>
+      console.log("Webhook set to", `${DOMAIN}/bot${process.env.BOT_TOKEN}`)
+    )
+    .catch(console.error);
+}
