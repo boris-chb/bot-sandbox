626069cac8bf631114897afa05cf0eabfa5dc5e3
Boris Ciobirca <boris.chobyrka@gmail.com>
2025-10-02 01:54:39 +0100

    fix: redis

--- src/queue/redis.ts
+++ src/queue/redis.ts
@@ -1,9 +1,12 @@
 import type { RedisOptions } from "ioredis";
 
-const host = process.env.REDIS_HOST || "127.0.0.1";
-const port = Number(process.env.REDIS_PORT) || 6379;
-
-export const connection: RedisOptions | string =
-  process.env.NODE_ENV === "production"
-    ? (process.env.REDIS_URL as string)
-    : ({ host, port } as RedisOptions);
+export const connection: RedisOptions =
+  process.env.NODE_ENV === "production" && process.env.REDIS_URL
+    ? {
+        // ioredis will handle the URL parsing
+        ...(process.env.REDIS_URL.startsWith("rediss://") && { tls: {} }),
+      }
+    : {
+        host: process.env.REDIS_HOST || "127.0.0.1",
+        port: Number(process.env.REDIS_PORT) || 6379,
+      };
