b5baf4d6e7c69fdecb4c6df1c69897b3d89575f6
Boris Ciobirca <boris.chobyrka@gmail.com>
2025-10-02 02:12:45 +0100

    fix: redis

--- src/queue/redis.ts
+++ src/queue/redis.ts
@@ -1,11 +1,11 @@
 import type { RedisOptions } from "ioredis";
 
 export const connection: RedisOptions = {
-  host: process.env.REDISHOST || "localhost",
-  port: Number(process.env.REDISPORT) || 6379,
-  password: process.env.REDIS_PASSWORD,
-  username: process.env.REDISUSER,
-  tls: {},
+  host: "interchange.proxy.rlwy.net",
+  port: 28900,
+  username: "default",
+  password: "YGkUkiWRSIutrgPLpsSjEuDKcRmIeiZp",
+  tls: {}, // because Railway public Redis requires TLS
 };
 
 console.log(connection);
