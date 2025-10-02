d9812b5104aced5cf97b7d6ae0379ea1242679a5
Boris Ciobirca <boris.chobyrka@gmail.com>
2025-10-02 02:05:00 +0100

    fix: redis

--- src/queue/redis.ts
+++ src/queue/redis.ts
@@ -1,3 +1,11 @@
 import type { RedisOptions } from "ioredis";
 
-export const connection: RedisOptions = {};
+export const connection: RedisOptions = {
+  host: process.env.REDISHOST || "localhost",
+  port: Number(process.env.REDISPORT) || 6379,
+  password: process.env.REDIS_PASSWORD,
+  username: process.env.REDISUSER,
+  tls: {},
+};
+
+console.log(connection);
